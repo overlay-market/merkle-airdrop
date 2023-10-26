c2af39e3c912c05f6632dee22b2dcafb6071a8d2
Ezequiel <eperez@itba.edu.ar>
2023-10-26 15:51:46 -0300

    feat: use external token in claim, and format code

--- contracts/src/MerkleClaimERC20.sol
+++ contracts/src/MerkleClaimERC20.sol
@@ -1,79 +1,80 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.0;
+pragma solidity 0.8.19;
 
 /// ============ Imports ============
 
-import { ERC20 } from "@solmate/tokens/ERC20.sol"; // Solmate: ERC20
-import { MerkleProof } from "@openzeppelin/utils/cryptography/MerkleProof.sol"; // OZ: MerkleProof
+import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
+import {MerkleProof} from "@openzeppelin/utils/cryptography/MerkleProof.sol";
 
 /// @title MerkleClaimERC20
-/// @notice ERC20 claimable by members of a merkle tree
 /// @author Anish Agnihotri <contact@anishagnihotri.com>
-/// @dev Solmate ERC20 includes unused _burn logic that can be removed to optimize deployment cost
-contract MerkleClaimERC20 is ERC20 {
+contract MerkleClaimERC20 {
+    /// ============ Immutable storage ============
 
-  /// ============ Immutable storage ============
+    /// @notice ERC20-claimee inclusion root
+    bytes32 public immutable merkleRoot;
 
-  /// @notice ERC20-claimee inclusion root
-  bytes32 public immutable merkleRoot;
+    /// @notice Contract address of airdropped token
+    IERC20 public immutable token;
 
-  /// ============ Mutable storage ============
+    /// ============ Mutable storage ============
 
-  /// @notice Mapping of addresses who have claimed tokens
-  mapping(address => bool) public hasClaimed;
+    /// @notice Mapping of addresses who have claimed tokens
+    mapping(address => bool) public hasClaimed;
 
-  /// ============ Errors ============
+    /// ============ Errors ============
 
-  /// @notice Thrown if address has already claimed
-  error AlreadyClaimed();
-  /// @notice Thrown if address/amount are not part of Merkle tree
-  error NotInMerkle();
+    /// @notice Thrown if address has already claimed
+    error AlreadyClaimed();
 
-  /// ============ Constructor ============
+    /// @notice Thrown if address/amount are not part of Merkle tree
+    error NotInMerkle();
 
-  /// @notice Creates a new MerkleClaimERC20 contract
-  /// @param _name of token
-  /// @param _symbol of token
-  /// @param _decimals of token
-  /// @param _merkleRoot of claimees
-  constructor(
-    string memory _name,
-    string memory _symbol,
-    uint8 _decimals,
-    bytes32 _merkleRoot
-  ) ERC20(_name, _symbol, _decimals) {
-    merkleRoot = _merkleRoot; // Update root
-  }
+    /// @notice Thrown if claim contract doesn't have enough tokens to payout
+    error NotEnoughRewards();
 
-  /// ============ Events ============
+    /// ============ Constructor ============
 
-  /// @notice Emitted after a successful token claim
-  /// @param to recipient of claim
-  /// @param amount of tokens claimed
-  event Claim(address indexed to, uint256 amount);
+    /// @notice Creates a new MerkleClaimERC20 contract
+    /// @param _token address of airdropped token
+    /// @param _merkleRoot merkle root of claimees
+    constructor(IERC20 _token, bytes32 _merkleRoot) {
+        token = _token;
+        merkleRoot = _merkleRoot;
+    }
 
-  /// ============ Functions ============
+    /// ============ Events ============
 
-  /// @notice Allows claiming tokens if address is part of merkle tree
-  /// @param to address of claimee
-  /// @param amount of tokens owed to claimee
-  /// @param proof merkle proof to prove address and amount are in tree
-  function claim(address to, uint256 amount, bytes32[] calldata proof) external {
-    // Throw if address has already claimed tokens
-    if (hasClaimed[to]) revert AlreadyClaimed();
+    /// @notice Emitted after a successful token claim
+    /// @param to recipient of claim
+    /// @param amount of tokens claimed
+    event Claim(address indexed to, uint256 amount);
 
-    // Verify merkle proof, or revert if not in tree
-    bytes32 leaf = keccak256(abi.encodePacked(to, amount));
-    bool isValidLeaf = MerkleProof.verify(proof, merkleRoot, leaf);
-    if (!isValidLeaf) revert NotInMerkle();
+    /// ============ Functions ============
 
-    // Set address to claimed
-    hasClaimed[to] = true;
+    /// @notice Allows claiming tokens if address is part of merkle tree
+    /// @param to address of claimee
+    /// @param amount of tokens owed to claimee
+    /// @param proof merkle proof to prove address and amount are in tree
+    function claim(address to, uint256 amount, bytes32[] calldata proof) external {
+        // Throw if address has already claimed tokens
+        if (hasClaimed[to]) revert AlreadyClaimed();
 
-    // Mint tokens to address
-    _mint(to, amount);
+        // Throw if the contract doesn't hold enough tokens for claimee
+        if (amount > token.balanceOf(address(this))) revert NotEnoughRewards();
 
-    // Emit claim event
-    emit Claim(to, amount);
-  }
+        // Verify merkle proof, or revert if not in tree
+        bytes32 leaf = keccak256(abi.encodePacked(to, amount));
+        bool isValidLeaf = MerkleProof.verify(proof, merkleRoot, leaf);
+        if (!isValidLeaf) revert NotInMerkle();
+
+        // Set address to claimed
+        hasClaimed[to] = true;
+
+        // Award tokens to address
+        token.transfer(to, amount);
+
+        // Emit claim event
+        emit Claim(to, amount);
+    }
 }
---
+++ contracts/src/OverlayV1Token.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/token/ERC20/ERC20.sol";
+import "@openzeppelin/access/AccessControlEnumerable.sol";
+
+import "./interfaces/IOverlayV1Token.sol";
+
+contract OverlayV1Token is IOverlayV1Token, AccessControlEnumerable, ERC20("Overlay", "OVL") {
+    /// @notice indicates whether transfers are allowed for everyone or only whitelisted addresses
+    bool public transfersLocked;
+
+    constructor() {
+        // Only whitelisted addresses can transfer by default
+        transfersLocked = true;
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    modifier onlyMinter() {
+        require(hasRole(MINTER_ROLE, msg.sender), "ERC20: !minter");
+        _;
+    }
+
+    modifier onlyBurner() {
+        require(hasRole(BURNER_ROLE, msg.sender), "ERC20: !burner");
+        _;
+    }
+
+    function mint(address _recipient, uint256 _amount) external onlyMinter {
+        _mint(_recipient, _amount);
+    }
+
+    function burn(uint256 _amount) external onlyBurner {
+        _burn(msg.sender, _amount);
+    }
+
+    function unlockTransfers() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        transfersLocked = false;
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256)
+        internal
+        view
+        override
+    {
+        require(
+            !transfersLocked || hasRole(TRANSFER_ROLE, from) || hasRole(TRANSFER_ROLE, to),
+            "ERC20: cannot transfer"
+        );
+    }
+}
